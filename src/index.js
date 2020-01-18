7910632e09115687d590b5d20b8951399ee1fa8f
唯影 <807781817@qq.com>
2020-01-18 15:08:53 +0800

    add

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import '@babel/polyfill'
+import avatar from './assets/images/avatar.jpg'
+
+const img = new Image()
+img.src = avatar
+
+const root = document.querySelector('#app')
+root.append(img)
+
+// import fn from './fn'
+// if (module.hot) {
+//   module.hor.accept('./fn', () => {
+//     fn()
+//   })
+// }
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  // sourceMap配置 -- 映射关系,对应开发环境中的js文件
+  // devtool: 'cheap-module-eval-source-map' // development
+  // devtool: 'cheap-module-source-map' // production
+  devtool: 'cheap-module-eval-source-map',
+  entry: {
+    index: './src/index.js',
+    main: './src/index.js'
+  },
+  // 开启web服务器,同时配置代码变化自动刷新页面
+  devServer: {
+    contentBase: './dist', // 默认的基础路径
+    // open: true, // 启动时自动打开页面
+    port: 8000, // 设置服务的端口
+    hot: true, // 开启热更新 Hot Module ReplaceMent(HMR)
+    hotOnly: true, // 即使HMR失效,浏览器也不重新加载
+    proxy: { // 代理配置
+      '/api': {
+        target: 'http://127.0.0.1:8080', // 接口域名
+        changeOrigin: true // 是否跨域
+      }
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.(jpg|png|gif|jpeg)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images/',
+          limit: 204800
+        }
+      }
+    }, {
+      test: /\.css$/, use: ['style-loader', 'css-loader']
+    }, { // es6转es5
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+      // options: {
+      //   presets: [['@babel/preset-env', {
+      //     useBuiltIns: 'usage' // 将用到的es6转为es5, 无需将没用到的es6语法转换打包
+      //   }]] // es6转es5必要插件
+      // }
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new htmlWebpackPlugin({
+      minify: { // 压缩HTML文件
+        removeComments: true, // 移除HTML中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true// 压缩内联css
+      },
+      template: 'public/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin() // HRM功能开启--热更新(样式等变动不会重新加载页面)
+  ],
+  output: {
+    publicPath: '/',
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
